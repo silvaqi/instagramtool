9dcbb1ab4b16bdf2428f81ff341979ca126e799a
bug <bug@core>
2023-01-30 10:41:01 -0300

    Initial commit

---
+++ prompt.js
@@ -0,0 +1,11 @@
+const prompt = require('prompt');
+
+prompt.start();
+
+prompt.get(
+    ['username', 'email'], function (err, result){
+        console.log('Command-line input received:');
+        console.log(' username: ' + result.username)
+        console.log(' email: ' + result.email)
+    }
+)
---
+++ promptwo.js
@@ -0,0 +1,20 @@
+const prompt = require('prompt')
+const axios = require('axios')
+
+prompt.start();
+
+prompt.get(
+    ['username'], function (err, result){
+        console.log(' Username ' + result.username)
+        axios.get('https://www.instagram.com/'+result.username+'/?__a=1&__d=dist').then( response =>{
+            console.log('===============Informations below=================')
+            console.log('Username: ' + response.data.graphql.user.full_name)
+            console.log('==================================================')
+            console.log('Biography: ' + response.data.graphql.user.biography)
+            console.log('==================================================')
+            console.log('Profile Picture: ' + response.data.graphql.user.profile_pic_url)
+            console.log('==================================================')
+            console.log('End. Made by J/Bug')
+        })
+    }
+)
